3c125754f0005bda6e86142b73e27eaf9fe543a4
ntack <nick@mana-partners.com>
2018-04-11 00:57:25 -0400

    added timer file,class

---
+++ Assets/Scripts/Util/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Timer
+{
+
+    private uint counter = 0;
+    private Dictionary<uint, List<Action>> eventsToFire = new Dictionary<uint, List<Action>>();
+    /// <summary>
+    /// Increment the global frame counter, and call any functions scheduled at the new frame
+    /// </summary>
+    public void incrementTimer()
+    {
+        counter++;
+        if (eventsToFire.ContainsKey(counter)){
+            eventsToFire[counter].ForEach(func => func());
+            eventsToFire.Remove(counter);
+        }
+    }
+
+    /// <summary>
+    /// Set a function to execute after (delay) frames.
+    /// </summary>
+    /// <param name="delay"></param>
+    /// <param name="func"></param>
+    public void setTimer(uint delay, Action func)
+    {
+        uint currentTime = counter;
+        uint timeToRun = currentTime + delay;
+        if (eventsToFire.ContainsKey(timeToRun))
+        {
+            eventsToFire[timeToRun].Add(func);
+        }
+        else {
+            eventsToFire.Add(timeToRun, [func]);
+        }
+        
+    }
+}
